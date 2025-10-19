6e2f3cd3d9e62270b95b9122368a0a77c2c3870b
Huy Khang <huykhang666@gmail.com>
2025-10-19 17:54:50 +0700

    Complete console

--- Game2048/Board.cpp
+++ Game2048/Board.cpp
@@ -1,6 +1,7 @@
 ﻿#include "Board.h"
 #include<algorithm>
 #include<sstream>
+#include<fstream>
 
 //Constructor
 Board::Board() : rng(std::random_device{}()), score(0) {
@@ -60,6 +61,7 @@ bool Board::canMove() {
 				return true;
 		}
 	}
+	return false;
 }
 
 // Di chuyển hàng sang trái
@@ -83,6 +85,7 @@ bool Board::moveRowLeft(std::array<int, SIZE>& rowVals) {
 		if (j > 0 && rowVals[j - 1] == rowVals[j] && lastMerge != j - 1) {
 			rowVals[j - 1] *= 2;
 			score += rowVals[j - 1];
+			rowVals[j] = 0;
 			lastMerge = j - 1;
 			moved = true;
 		}
@@ -109,5 +112,102 @@ bool Board::move(Direction dir) {
 			}
 		}
 		break;
+
+	case Direction::RIGHT:
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				tempRow[j] = grid[i][SIZE - 1 - j].getValue();
+			}
+
+			moved |= moveRowLeft(tempRow);
+
+			for (int j = 0; j < SIZE; j++) {
+				grid[i][SIZE - 1 - j].setValue(tempRow[j]);
+			}
+		}
+		break;
+
+	case Direction::UP:
+		for (int j = 0; j < SIZE; j++) {
+			for (int i = 0; i < SIZE; i++) {
+				tempRow[i] = grid[i][j].getValue();
+			}
+
+			moved |= moveRowLeft(tempRow);
+
+			for (int i = 0; i < SIZE; i++) {
+				grid[i][j].setValue(tempRow[i]);
+			}
+		}
+		break;
+
+	case Direction::DOWN:
+		for (int j = 0; j < SIZE; j++) {
+			for (int i = 0; i < SIZE; i++) {
+				tempRow[i] = grid[SIZE - 1 - i][j].getValue();
+			}
+
+			moved |= moveRowLeft(tempRow);
+
+			for (int i = 0; i < SIZE; i++) {
+				grid[i][j].setValue(tempRow[i]);
+			}
+		}
+		break;
+	default:
+		break;
 	}
+
+	if (moved)
+		spawnRandomTile();
+
+	return moved;
+}
+
+//In bảng ra chuỗi
+std::string Board::toString() const {
+	std::ostringstream oss;
+	oss << "Score: " << score << "\n";
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			if (grid[i][j].getValue() == 0) {
+				oss << std::setw(4) << ".";
+			}
+			else {
+				oss << std::setw(4) << grid[i][j].getValue();
+			}
+		}
+		oss << "\n";
+	}
+	return oss.str();
+}
+
+//Lưu game
+void Board::saveToFile(const std::string& filename) const {
+	std::ofstream file(filename);
+	if (!file) return;
+
+	file << score << "\n";
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			file << grid[i][j].getValue() << " ";
+		}
+		file << "\n";
+	}
+}
+
+//Load game
+bool Board::loadFromFile(const std::string& filename) {
+	std::fstream file(filename);
+	if (!file) return false;
+
+	file >> score;
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			int v;
+			file >> v;
+			grid[i][j].setValue(v);
+		}
+	}
+	return true;
 }
---
+++ Game2048/Game.cpp
@@ -0,0 +1,97 @@
+﻿#include "Game.h"
+#include<conio.h>
+
+//Constructor
+Game::Game() : state(GameState::MENU) {}
+
+//Hàm run game chạy đến khi ấn exit
+void Game::run() {
+	while (state != GameState::EXIT) {
+		switch (state) {
+		case GameState::MENU: 
+			renderMenu();
+			break;
+		case GameState::PLAYING:
+			renderGame();
+			break;
+		case GameState::GAME_OVER:
+			renderGameOver();
+			break;
+		default: break;
+		}
+	}
+}
+
+//Hiển thị MENU chính
+void Game::renderMenu() {
+	system("cls");
+	std::cout << "===== 2048 GAME ==== \n";
+	std::cout << "1. Start New Game \n";
+	std::cout << "2. Load Game \n";
+	std::cout << "3. Exit \n";
+	std::cout << "===================== \n";
+	std::cout << "Choose: ";
+
+	int choice;
+	std::cin >> choice;
+
+	switch (choice) {
+	case 1:
+		board.reset();
+		state = GameState::PLAYING;
+		break;
+
+	case 2:
+		if (board.loadFromFile("save.txt"))
+			std::cout << "Loaded successfully! \n";
+		else
+			std::cout << "No save file found. \n";
+		system("pause");
+		state = GameState::PLAYING;
+		break;
+	case 3:
+		state = GameState::EXIT;
+		break;
+	default:
+		break;
+	}
+}
+
+//Xử lí khi đang chơi
+void Game::renderGame() {
+	system("cls");
+	std::cout << board.toString();
+	std::cout << "\nUse W/A/S/D to move. Press Q to quit, L to save.\n";
+
+	char key = _getch();
+	Direction dir = Direction::NONE;
+
+	switch (tolower(key)) {
+	case 'w': dir = Direction::UP; break;
+	case 'a': dir = Direction::LEFT; break;
+	case 's': dir = Direction::DOWN; break;
+	case 'd': dir = Direction::RIGHT; break;
+	case 'l': board.saveToFile("save.txt"); return;
+	case 'q': state = GameState::EXIT; return;
+	default: return;
+	}
+
+	if (board.move(dir)) {
+		board.spawnRandomTile();
+	}
+
+	if (!board.canMove()) {
+		state = GameState::GAME_OVER;
+	}
+}
+
+void Game::renderGameOver() {
+	system("cls");
+	std::cout << "===== GAME OVER ===== \n";
+	std::cout << board.toString();
+	std::cout << "\n Press M to return to menu to menu or Q to quit \n";
+
+	char key = _getch();
+	if (tolower(key) == 'm') state = GameState::MENU;
+	else state = GameState::EXIT;
+}
